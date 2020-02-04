39446dd9c7982043be41eb91901f845713289145
andrés castro araújo <acastroaraujo@gmail.com>
2020-02-04 17:11:59 -0500

    update

--- R/custom_themes.R
+++ R/custom_themes.R
@@ -11,7 +11,7 @@ NULL
 #' @return a custom ggplot2 theme
 #' @export
 #'
-theme_custom <- function(...) {
+theme_custom <- function(base_family = "Roboto", ...) {
   theme_minimal(...) %+replace%
     theme(
       plot.title = element_text(face = "bold"),
@@ -20,3 +20,20 @@ theme_custom <- function(...) {
       strip.background = element_rect(fill = "gray80", color = "gray80")
     )
 }
+
+
+theme_super_minimal <- function(base_family = "Roboto", fill = "#fffff8", ...) {
+  theme_minimal(base_line_size = 0, ...) %+replace%
+    theme(
+      plot.title = element_text(face = "bold", margin = margin(0, 0, 5, 0), hjust = 0),
+      plot.subtitle = element_text(margin = margin(0, 0, 5, 0), hjust = 0),
+      plot.background = element_rect(fill = fill, size = 0),
+      panel.grid.minor = element_blank(),
+      panel.grid.major = element_blank(),
+      axis.title.x = element_text(margin = margin(15, 0, 0, 0)),
+      axis.title.y = element_text(angle = 90, margin = margin(0, 15, 0, 0))
+      )
+}
+
+
+
