682e8dfe919e8fc7452f1fea25a3fe3f6dc0ecea
acastroaraujo <acastroaraujo@gmail.com>
2020-03-05 00:04:46 -0500

    update

--- R/custom_themes.R
+++ R/custom_themes.R
@@ -3,13 +3,15 @@ NULL
 
 
 #' @export
-theme_custom <- function(base_family = "Roboto", ...) {
+theme_custom <- function(base_family = "Roboto", fill = "#fffff8", ...) {
   theme_minimal(...) %+replace%
     theme(
-      plot.title = element_text(face = "bold", hjust = 0),
-      panel.grid.minor = element_blank(),
-      panel.grid.major = element_line(size = 0.2),
-      strip.background = element_rect(fill = "gray80", color = "gray80")
+      plot.title = element_text(face = "bold", margin = margin(0, 0, 5, 0), hjust = 0, size = 13),
+      plot.subtitle = element_text(face = "italic", margin = margin(0, 0, 5, 0), hjust = 0),
+      plot.background = element_rect(fill = fill, size = 0), complete = TRUE,
+      axis.title.x = element_text(margin = margin(15, 0, 0, 0)),
+      axis.title.y = element_text(angle = 90, margin = margin(0, 20, 0, 0)),
+      strip.text = element_text(face = "italic")
     )
 }
 
