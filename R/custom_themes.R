f9af3fc1dca370395cc900d7a22df4ecb5d13746
acastroaraujo <acastroaraujo@gmail.com>
2020-03-30 19:42:30 -0400

    update themes

--- R/custom_themes.R
+++ R/custom_themes.R
@@ -1,9 +1,9 @@
-#' @importFrom ggplot2 %+replace% theme theme_grey theme_minimal theme_bw
+#' @importFrom ggplot2 %+replace% theme theme_grey theme_minimal theme_bw margin element_text element_rect element_line
 NULL
 
 
 #' @export
-theme_custom <- function(base_family = "Roboto", fill = "#fffff8", ...) {
+theme_custom <- function(base_family = "Roboto", fill = "white", ...) {
   theme_minimal(...) %+replace%
     theme(
       plot.title = element_text(face = "bold", margin = margin(0, 0, 5, 0), hjust = 0, size = 13),
@@ -15,30 +15,24 @@ theme_custom <- function(base_family = "Roboto", fill = "#fffff8", ...) {
     )
 }
 
-
 #' @export
-theme_super_minimal <- function(base_family = "Roboto", fill = "#fffff8", ...) {
-  theme_minimal(base_line_size = 0, ...) %+replace%
+theme_blog <- function(base_family = "Verdana", base_line_size = 1, fill = "#FDF6E4", ...) {
+  theme_minimal(base_line_size = base_line_size, ...) %+replace%
     theme(
       plot.title = element_text(face = "bold", margin = margin(0, 0, 5, 0), hjust = 0, size = 13),
       plot.subtitle = element_text(face = "italic", margin = margin(0, 0, 5, 0), hjust = 0),
       plot.background = element_rect(fill = fill, size = 0), complete = TRUE,
-      axis.title.x = element_text(margin = margin(15, 0, 0, 0)),
-      axis.title.y = element_text(angle = 90, margin = margin(0, 20, 0, 0)),
-      strip.text = element_text(face = "italic")
-      )
-}
-
-#' @export
-theme_blog <- function(base_family = "Verdana", fill = "#FDF6E4", ...) {
-  theme_minimal(base_line_size = 0, ...) %+replace%
-    theme(
-      plot.title = element_text(face = "bold", margin = margin(0, 0, 5, 0), hjust = 0, size = 13),
-      plot.subtitle = element_text(face = "italic", margin = margin(0, 0, 5, 0), hjust = 0),
-      plot.background = element_rect(fill = fill, size = 0), complete = TRUE,
+      panel.grid = element_line(color = "white"),
       axis.title.x = element_text(margin = margin(15, 0, 0, 0)),
       axis.title.y = element_text(angle = 90, margin = margin(0, 20, 0, 0)),
       strip.text = element_text(face = "italic")
     )
 }
+
+#' @export
+theme_graph <- function() {
+  theme_blog(base_line_size = 0) %+replace%
+  theme(axis.title = element_blank(), axis.text = element_blank())
+}
+
 
