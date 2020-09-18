2e60cebd7575ae90cf830db770e895f36943b988
acastroaraujo <acastroaraujo@gmail.com>
2020-09-17 23:45:18 -0400

    geom_norm_ellipse

--- R/custom_themes.R
+++ R/custom_themes.R
@@ -36,4 +36,26 @@ theme_graph <- function() {
   theme(axis.title = element_blank(), axis.text = element_blank())
 }
 
+#' @export
+geom_norm_ellipse <- function(data = NULL, fill = "#333333", color = NA, size = 1/3, segments = 1e3) {
+
+  levels <- c(seq(from = 1/10, to = 9/10, by = 1/10), .99)
+
+  geom_ellipse <- function(x, type = "norm", fill, size, segments, color) {
+
+    stat_ellipse(
+      data = data,
+      type = type,
+      level = x,
+      geom = "polygon", alpha = 1/8,
+      fill = fill,
+      size = size,
+      segments = segments,
+      color = color
+    )
+  }
+
+  lapply(levels, geom_ellipse, fill = fill, color = color, segments = segments, size = size)
+
+}
 
